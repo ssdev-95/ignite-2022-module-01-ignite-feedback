dffe09698cc2ef16ee149d78f5f51d92ea124324
Salomao S <souza95salomao@gmail.com>
2022-06-12 02:36:25 +0000

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="app">
+			<Header/>
+			{/*<Sidebar />
+			<Posts />*/}
+    </div>
+  )
+}
+
+export default App
